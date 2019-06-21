db1c87273ce03134d3b9260e054c581d2a76896e
Xavi <xavi@geekshubs.com>
2019-06-21 11:42:21 +0200

    CRUD COMPLETE

--- src/BlogBundle/Controller/PostController.php
+++ src/BlogBundle/Controller/PostController.php
@@ -11,15 +11,20 @@ use BlogBundle\Entity\Post;
  * @Route("/post")
  */
 class PostController extends Controller
+
 {
+    private $em;
+
+    public function __construct()
+    {
+       
+    }
 
     /**
      * @Route("/add")
      */
     public function addAction(){
        
-        //Recuperamos el Entity Manager
-        $em = $this->getDoctrine()->getManager();
         
         //Creamos la entidad
         $post = new Post();
@@ -30,7 +35,7 @@ class PostController extends Controller
         $post->setIduser(1);
 
         //Persistimos la entidad
-
+        $em = $this->getDoctrine()->getManager();
         $em->persist($post);
         $em->flush();
 
@@ -57,6 +62,7 @@ class PostController extends Controller
     public function getAllFilterAction(){
 
         //Recuperar el Manager
+        
         $em = $this->getDoctrine()->getManager();
         $query = $em->createQuery(
             'SELECT p FROM BlogBundle:Post p'
@@ -75,4 +81,65 @@ class PostController extends Controller
         $post = $repository->find($id);
         return $this->render('@Blog/Default/post.html.twig',['post'=>$post]);
     }
+    /**
+     * @Route("/findtitle/{title}")
+     */
+    public function getPostByTitle($title)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $repository= $em->getRepository("BlogBundle:Post");
+        $post = $repository->findBy(array('title'=>"Prueba",
+                                          'tag'=>'tag'));
+        if (!$post){
+            return new Response("No existe el post");
+        }
+        return $this->render('@Blog/Default/posts.html.twig',['posts'=>$post]);
+    }
+     /**
+     * @Route("/findquery/{title}")
+     */
+    public function getPostByQuery($title)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $repository = $em->getRepository('BlogBundle:Post');
+        $query = $repository->createQueryBuilder('p')
+        ->where('p.title LIKE :title')
+        ->setParameter('title',$title)
+        ->getQuery();
+        $post = $query->getResult();
+        return $this->render('@Blog/Default/posts.html.twig',['posts'=>$post]);
+    }
+     /**
+     * @Route("/deletePost/{id}")
+     */
+    public function deletePost($id)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $repository = $em->getRepository('BlogBundle:Post');
+        $post = $repository->find($id);
+        if (!$post){
+            return new Response("No existe el post");
+        }
+        $em->remove($post);
+        $em->flush();
+        return new Response("Post Eliminado ->".$post->getId());
+
+    }
+     /**
+     * @Route("/updatePost/{id}")
+     */
+    public function updatePost($id)
+    {
+        $em=$this->getDoctrine()->getManager();
+        $post = $em->getRepository("BlogBundle:Post")->find($id);
+        if (!$post){
+            return new Response("No existe el post");
+        }
+        $post->setTitle("OtroPost");
+        $em->flush();
+        //return new Response("Post Actualizado ->".$post->getTitle());
+        return $this->redirect('/blog/post/getAll');
+    }
+
+
 }
